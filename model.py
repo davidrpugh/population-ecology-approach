b13ec228056784bc6dce5e99093afe5b935bcca7
David R. Pugh <drobert.pugh@gmail.com>
2014-07-13 12:08:50 -0400

    BUG FIX: steady_state property now depends on _initial_condition.

--- model.py
+++ model.py
@@ -10,6 +10,8 @@ from traits.api import (Array, cached_property, Dict, Float, HasPrivateTraits,
                         Property, Str)
 
 import wrapped_symbolics
+import wrapped_transformed_symbolics
+
 
 class Model(HasPrivateTraits):
     """Base class representing the model of Pugh-Schaefer-Seabright."""
@@ -20,20 +22,21 @@ class Model(HasPrivateTraits):
 
     _female_alleles_constraint = Property
 
+    _initial_guess = Array
+
     _male_alleles_constraint = Property
 
-    initial_guess = Array 
+    initial_guess = Property(Array) 
 
     params = Dict(Str, Float)
 
     solver_kwargs = Dict(Str, Float)
 
-    steady_state = Property(depends_on=['initial_guess, params'])
+    steady_state = Property(depends_on=['_initial_guess, params'])
 
     def _get__bound_constraints(self):
         """Population shares must be in [0,1]."""
-        eps = 1e-15
-        return [(eps, 1 - eps) for i in range(8)]
+        raise NotImplementedError
 
     def _get__equality_constraints(self):
         """Population shares of male and female alleles must sum to one."""
@@ -41,25 +44,31 @@ class Model(HasPrivateTraits):
 
     def _get__female_alleles_constraint(self):
         """Female allele population shares must sum to one."""
-        cons = lambda X: 1 - np.sum(X[4:])
-        return {'type': 'eq', 'fun': cons}
+        raise NotImplementedError
 
     def _get__male_alleles_constraint(self):
         """Male allele population shares must sum to one."""
-        cons = lambda X: 1 - np.sum(X[:4])
-        return {'type': 'eq', 'fun': cons}
+        raise NotImplementedError
+
+    def _get_initial_guess(self):
+        """Return initial guess of the equilibrium population shares."""
+        return self._initial_guess
 
     @cached_property
     def _get_steady_state(self):
         """Compute the steady state for the model."""
         result = optimize.minimize(self._objective, 
-                                   x0=self.initial_guess,
+                                   x0=self._initial_guess,
                                    method='SLSQP', 
                                    jac=self._jacobian,
                                    bounds=self._bound_constraints,
                                    constraints=self._equality_constraints,
                                    **self.solver_kwargs)
         return result
+
+    def _set_initial_guess(self, value):
+        """Specify the initial guess of the equilibrium population shares."""
+        self._initial_guess = value
 
     def _jacobian(self, X):
         """Jacobian of the objective function."""
@@ -73,13 +82,11 @@ class Model(HasPrivateTraits):
 
     def _residual(self, X):
         """Model steady state is a root of this non-linear system."""
-        resid = wrapped_symbolics.residual(*X, **self.params)
-        return np.array(resid)
+        raise NotImplementedError
 
     def _residual_jacobian(self, X):
         """Returns the Jacobian of the model residual."""
-        jac = wrapped_symbolics.residual_jacobian(*X, **self.params)
-        return np.array(jac)
+        raise NotImplementedError
 
     def F(self, X):
         """Equation of motion for population allele shares."""
@@ -104,3 +111,68 @@ class Model(HasPrivateTraits):
 
         return traj
     
+
+class StandardModel(Model):
+    """Class representing the model of Pugh-Schaefer-Seabright."""
+
+    def _get__bound_constraints(self):
+        """Population shares must be in [0,1]."""
+        eps = 1e-15
+        return [(eps, 1 - eps) for i in range(8)]
+
+    def _get__female_alleles_constraint(self):
+        """Female allele population shares must sum to one."""
+        cons = lambda X: 1 - np.sum(X[4:])
+        return {'type': 'eq', 'fun': cons}
+
+    def _get__male_alleles_constraint(self):
+        """Male allele population shares must sum to one."""
+        cons = lambda X: 1 - np.sum(X[:4])
+        return {'type': 'eq', 'fun': cons}
+
+    def _residual(self, X):
+        """Model steady state is a root of this non-linear system."""
+        resid = wrapped_symbolics.residual(*X, **self.params)
+        return np.array(resid)
+
+    def _residual_jacobian(self, X):
+        """Returns the Jacobian of the model residual."""
+        jac = wrapped_symbolics.residual_jacobian(*X, **self.params)
+        return np.array(jac)
+
+ 
+class TransformedModel(Model):
+    """Transformed version of the Pugh-Schaefer-Seabright model."""
+
+    def _get__bound_constraints(self):
+        """Population shares must be in [0,1]."""
+        return [(None, None) for i in range(8)]
+
+    def _get__equality_constraints(self):
+        """Population shares of male and female alleles must sum to one."""
+        return [self._male_alleles_constraint, self._female_alleles_constraint]
+
+    def _get__female_alleles_constraint(self):
+        """Female allele population shares must sum to one."""
+        cons = lambda X: 1 - np.sum((1 / (1 + np.exp(-X))[4:]))
+        return {'type': 'eq', 'fun': cons}
+
+    def _get__initial_guess(self):
+        """Apply logistic transformation of variables to initial guess."""
+        transform_initial_guess = np.log(initial_guess / (1 - initial_guess))
+        return transform_initial_guess
+
+    def _get__male_alleles_constraint(self):
+        """Male allele population shares must sum to one."""
+        cons = lambda X: 1 - np.sum((1 / (1 + np.exp(-X))[:4]))
+        return {'type': 'eq', 'fun': cons}
+
+    def _residual(self, X):
+        """Model steady state is a root of this non-linear system."""
+        resid = wrapped_transformed_symbolics.residual(*X, **self.params)
+        return np.array(resid)
+
+    def _residual_jacobian(self, X):
+        """Returns the Jacobian of the model residual."""
+        jac = wrapped_transformed_symbolics.residual_jacobian(*X, **self.params)
+        return np.array(jac)
