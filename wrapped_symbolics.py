03ac1d0fc5ee348f1976c17693c5afafcba10bf7
David R. Pugh <drobert.pugh@gmail.com>
2014-07-13 19:24:45 -0400

    PEP 8 certified.

--- wrapped_symbolics.py
+++ wrapped_symbolics.py
@@ -13,11 +13,12 @@ dA, da = symbolics.female_signaling_probs
 eA, ea = symbolics.male_screening_probs
 PiaA, PiAA, Piaa, PiAa = symbolics.matching_payoffs
 
-# wraps the symbolic model system and its Jacobian for use in numerical analysis
-tmp_args = (mGA, mGa, mgA, mga, fGA, fGa, fgA, fga, dA, da, eA, ea, PiaA, PiAA, Piaa, PiAa)
-model_system = sp.lambdify(tmp_args, symbolics.model_system, modules='numpy')
-model_jacobian = sp.lambdify(tmp_args, symbolics.model_jacobian, modules='numpy')
+# wraps the symbolic system and its Jacobian for use in numerical analysis
+tmp_args = (mGA, mGa, mgA, mga, fGA, fGa, fgA, fga,
+            dA, da, eA, ea, PiaA, PiAA, Piaa, PiAa)
+model_system = sp.lambdify(tmp_args, symbolics.model_system, 'numpy')
+model_jacobian = sp.lambdify(tmp_args, symbolics.model_jacobian, 'numpy')
 
 # wraps the symbolic residual and its Jacobian for use in numerical analysis
-residual = sp.lambdify(tmp_args, symbolics.residual, modules='numpy')
-residual_jacobian = sp.lambdify(tmp_args, symbolics.residual_jacobian, modules='numpy')
+residual = sp.lambdify(tmp_args, symbolics.residual, 'numpy')
+residual_jacobian = sp.lambdify(tmp_args, symbolics.residual_jacobian, 'numpy')
