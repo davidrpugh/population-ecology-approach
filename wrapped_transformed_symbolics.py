9f9c56376652a34901e2324526b899464f7ee74d
David R. Pugh <drobert.pugh@gmail.com>
2014-07-13 13:38:19 -0400

    Should have added this to the transformed-model branch before the merge.

---
+++ wrapped_transformed_symbolics.py
@@ -0,0 +1,31 @@
+"""
+Converts symbolic expressions defining the model into callable NumPy functions.
+
+"""
+import sympy as sp 
+
+import symbolics
+import transformed_symbolics
+
+# extract the important variables
+fGA_tilde, fGa_tilde, fgA_tilde, fga_tilde = transformed_symbolics.female_allele_shares
+mGA_tilde, mGa_tilde, mgA_tilde, mga_tilde = transformed_symbolics.male_allele_shares
+dA, da = symbolics.female_signaling_probs
+eA, ea = symbolics.male_screening_probs
+PiaA, PiAA, Piaa, PiAa = symbolics.matching_payoffs
+
+# wraps the symbolic model system and its Jacobian for use in numerical analysis
+tmp_args = (mGA_tilde, mGa_tilde, mgA_tilde, mga_tilde, 
+            fGA_tilde, fGa_tilde, fgA_tilde, fga_tilde, 
+            dA, da, eA, ea, PiaA, PiAA, Piaa, PiAa)
+model_system = sp.lambdify(tmp_args, transformed_symbolics.model_system, 
+                           modules='numpy')
+model_jacobian = sp.lambdify(tmp_args, transformed_symbolics.model_jacobian, 
+                             modules='numpy')
+
+# wraps the symbolic residual and its Jacobian for use in numerical analysis
+residual = sp.lambdify(tmp_args, transformed_symbolics.residual, 
+                       modules='numpy')
+residual_jacobian = sp.lambdify(tmp_args, transformed_symbolics.residual_jacobian, 
+                                modules='numpy')
+
