5a2b02d7845c35f5c0640298541bc11f3d003b7d
David R. Pugh <drobert.pugh@gmail.com>
2014-07-24 11:48:19 -0400

    Added an example for imperfect screening.

---
+++ examples/random-signaling/imperfect_screening_equilibria.py
@@ -0,0 +1,66 @@
+import sys
+sys.path.append('../../')
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from package import model
+
+# females send random signals, but males screen with imperfect screening
+eps = 1e-3
+params = {'dA': 0.5, 'da': 0.5, 'eA': 1.0 - eps, 'ea': 1.0 - eps,
+          'PiaA': 6.0, 'PiAA': 5.0, 'Piaa': 4.0, 'PiAa': 3.0}
+
+# create an array of initial guesses for root finder
+N = 100
+prng = np.random.RandomState(42)
+initial_males = prng.dirichlet(np.ones(4), size=N)
+initial_females = initial_males
+initial_guesses = np.hstack((initial_males, initial_females))
+
+# create an instance of the model
+example = model.Model(params=params,
+                      solver_kwargs={'tol': 1e-12})
+
+# initialize a storage container
+steady_states = np.empty((N, 8))
+
+# compute the steady state of the model for different initial guesses
+fig, ax = plt.subplots()
+ind = np.arange(-0.5, 7.5)
+
+for i in range(N):
+
+    # extract initial guess
+    example.initial_guess = initial_guesses[i]
+
+    # solve the nonlinear system
+    tmp_result = example.steady_state
+
+    if tmp_result.success and np.isclose(tmp_result.fun, 0.0, atol=1e-6):
+        steady_states[i] = tmp_result.x
+        bar_list = ax.bar(left=ind, height=tmp_result.x, width=1.0, alpha=0.05)
+
+        # Set different color for Altruistic genotypes
+        bar_list[0].set_color('r')
+        bar_list[2].set_color('r')
+        bar_list[4].set_color('r')
+        bar_list[6].set_color('r')
+
+    else:
+        steady_states[i] = np.nan
+
+    print 'Done with %i out of %i.' % (i, N)
+
+# axes , labels, title, etc
+ax.set_xlim(-1.0, 8.5)
+ax.set_xticks(np.arange(8))
+male_labels = ('$m_{GA}$', '$m_{Ga}$', '$m_{gA}$', '$m_{ga}$')
+female_labels = ('$f_{GA}$', '$f_{Ga}$', '$f_{gA}$', '$f_{ga}$')
+ax.set_xticklabels(male_labels + female_labels)
+ax.set_ylim(0, 1)
+ax.set_title('Putative equilibrium population shares', fontsize=20)
+
+# save and display the figure
+fig.savefig('../../images/random-signaling/imperfect_screening_equilibria.png')
+plt.show()
---
+++ examples/random-signaling/imperfect_screening_trajectories_1.py
@@ -0,0 +1,71 @@
+import sys
+sys.path.append('../../')
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from package import model
+
+# females send random signals, but males screen imperfectly
+#eps = 8.0e-1
+params = {'dA': 0.5, 'da': 0.5, 'eA': 0.41666667, 'ea': 0.5,
+          'PiaA': 6.0, 'PiAA': 5.0, 'Piaa': 4.0, 'PiAa': 3.0}
+
+# create an array of initial guesses for root finder
+N = 500
+prng = np.random.RandomState(42)
+initial_males = prng.dirichlet(np.ones(4), size=N)
+initial_females = initial_males
+initial_guesses = np.hstack((initial_males, initial_females))
+
+# create an instance of the model
+example = model.Model(params=params,
+                      solver_kwargs={'tol': 1e-12})
+
+fig, axes = plt.subplots(1, 2, figsize=(12, 8))
+
+for i in range(N):
+
+    # extract initial guess
+    example.initial_guess = initial_guesses[i]
+    tmp_traj = example.simulate(initial_condition=example.initial_guess, T=500)
+
+    # male allele trajectories
+    m_GA, = axes[0].plot(tmp_traj[0], color='b', alpha=0.05)
+    m_Ga, = axes[0].plot(tmp_traj[1], color='g', alpha=0.05)
+    m_gA, = axes[0].plot(tmp_traj[2], color='r', alpha=0.05)
+    m_ga, = axes[0].plot(tmp_traj[3], color='c', alpha=0.05)
+
+    # female allele trajectories
+    f_GA, = axes[1].plot(tmp_traj[4], color='b', alpha=0.05)
+    f_Ga, = axes[1].plot(tmp_traj[5], color='g', alpha=0.05)
+    f_gA, = axes[1].plot(tmp_traj[6], color='r', alpha=0.05)
+    f_ga, = axes[1].plot(tmp_traj[7], color='c', alpha=0.05)
+
+    print "Done with %i out of %i." % (i, N)
+
+# axes, labels, title, legend, etc
+axes[0].set_ylim(0, 1)
+axes[0].set_ylabel('Population shares', family='serif', fontsize=15)
+axes[0].set_title('Males', family='serif', fontsize=20)
+legend_0 = axes[0].legend([m_GA, m_Ga, m_gA, m_ga],
+                          [r'$m_{GA}$', r'$m_{Ga}$', r'$m_{gA}$', r'$m_{ga}$'],
+                          loc=0, frameon=False)
+
+# want legend lines to be solid!
+for line_obj in legend_0.legendHandles:
+    line_obj.set_alpha(1.0)
+
+axes[1].set_ylim(0, 1)
+axes[1].set_title('Females', family='serif', fontsize=20)
+legend_1 = axes[1].legend([f_GA, f_Ga, f_gA, f_ga],
+                          [r'$f_{GA}$', r'$f_{Ga}$', r'$f_{gA}$', r'$f_{ga}$'],
+                          loc=0, frameon=False)
+
+# want legend lines to be solid!
+for line_obj in legend_1.legendHandles:
+    line_obj.set_alpha(1.0)
+
+# save and display the figure
+fig.savefig('../../images/random-signaling/imperfect_screening_trajectories_1.png')
+plt.show()
