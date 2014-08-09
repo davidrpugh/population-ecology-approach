5bb681f6ee6b5418be4c440dfc8c037491fad872
David R. Pugh <drobert.pugh@gmail.com>
2014-08-09 17:00:40 +0100

    Refactored the parameter sweep code.

--- examples/parameter_sweep.py
+++ examples/parameter_sweep.py
@@ -6,20 +6,31 @@ import numpy as np
 
 from package import model
 
-# define an array of initial conditions S = mGA = fGA
-N = 5
-mGA0 = np.linspace(0, 1, N).reshape((N, 1))
+# define a model object
+mod = model.Model()
+
+# define an array of initial conditions S_A = mGA = fGA
+step = 0.25
+mGA0 = np.arange(0, 1, step)
 mga0 = 1 - mGA0
-initial_males = np.hstack((mGA0, np.zeros((N, 2)), mga0))
+
+tup = (mGA0[:, np.newaxis], np.zeros((mGA0.size, 2)), mga0[:, np.newaxis])
+initial_males = np.hstack(tup)
 initial_females = initial_males
+
 initial_conditions = np.hstack((initial_males, initial_females))
 
-# define some signaling probs
-signaling_probs = np.linspace(0, 1, N)
-screening_probs = signaling_probs
+# define some signaling and screening params
+step = 0.1
+signaling_probs = np.arange(0, 1+step, step)
+screening_probs = np.arange(0.5, 1+step, step)
 
-# storage container
-results = np.empty((N, N, N, N, N, 8))
+# storage container for results
+N_signal = signaling_probs.size
+N_screen = screening_probs.size
+N_initial = initial_conditions.shape[0]
+results_shape = (N_signal, N_signal, N_screen, N_screen, N_initial, 8)
+results = np.empty(results_shape)
 
 for i, dA in enumerate(signaling_probs):
     for j, da in enumerate(signaling_probs):
@@ -30,12 +41,10 @@ for i, dA in enumerate(signaling_probs):
                     # fix the model parameters
                     tmp_params = {'dA': dA, 'da': da, 'eA': eA, 'ea': ea,
                                   'PiaA': 6.0, 'PiAA': 5.0, 'Piaa': 4.0, 'PiAa': 3.0}
-
-                    # create an instance of the model
-                    tmp_example = model.Model(params=tmp_params)
+                    mod.params = tmp_params
 
                     # simulate the model to find the equilibrium
-                    tmp_traj = tmp_example.simulate(initial_condition, rtol=1e-4)
+                    tmp_traj = mod.simulate(initial_condition, rtol=1e-4)
 
                     # store the results
                     results[i, j, k, l, m, :] = tmp_traj[:, -1]
