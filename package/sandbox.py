5ae85a65b71bcf3a8df550b530cad93d7c448e85
David R. Pugh <drobert.pugh@gmail.com>
2014-08-27 22:25:14 -0400

    Added a function for computing payoffs.

--- package/sandbox.py
+++ package/sandbox.py
@@ -1,4 +1,9 @@
-# define the allele indicator functions
+import sympy as sym
+
+# Payoff parameters (from a Prisoner's dilemma)
+PiaA, PiAA, Piaa, PiAa = matching_payoffs = sym.var('PiaA, PiAA, Piaa, PiAa')
+
+
 def iscarrier_G(i):
     """Return True if adult carries the G allele."""
     if i in [0, 1]:
@@ -23,3 +28,11 @@ def iscarrier_A(i):
 def iscarrier_a(i):
     """Return True if adult carries the A allele."""
     return 1 - iscarrier_A(i)
+
+
+def get_payoff(i, j):
+    payoff = (iscarrier_a(i) * iscarrier_A(j) * PiaA +
+              iscarrier_A(i) * iscarrier_A(j) * PiAA +
+              iscarrier_a(i) * iscarrier_a(j) * Piaa +
+              iscarrier_A(i) * iscarrier_a(j) * PiAa)
+    return payoff
