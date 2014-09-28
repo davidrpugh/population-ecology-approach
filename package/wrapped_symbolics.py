c2da2902e33a96b2beeb12c3ef4018f5c5d77733
David R. Pugh <drobert.pugh@gmail.com>
2014-09-28 11:06:12 -0400

    Updated to include new symbolics folder.

--- package/wrapped_symbolics.py
+++ package/wrapped_symbolics.py
@@ -5,36 +5,36 @@ Converts symbolic expressions defining the model into callable NumPy functions.
 import numpy as np
 import sympy as sp
 
-import sandbox
+import symbolics
 
 # extract the important variables
-c = sandbox.fecundity_factor
-dA, da = sandbox.female_signaling_probs
-eA, ea = sandbox.male_screening_probs
-PiaA, PiAA, Piaa, PiAa = sandbox.prisoners_dilemma_payoffs
+c = symbolics.fecundity_factor
+dA, da = symbolics.female_signaling_probs
+eA, ea = symbolics.male_screening_probs
+PiaA, PiAA, Piaa, PiAa = symbolics.prisoners_dilemma_payoffs
 
 # wrapped symbolic expressions for sizes of the adoption pools
-tmp_args = (sandbox.girls, c, dA, da, eA, ea, PiaA, PiAA, Piaa, PiAa)
+tmp_args = (symbolics.girls, c, dA, da, eA, ea, PiaA, PiAA, Piaa, PiAa)
 altruist_adoption_pool = sp.lambdify(tmp_args,
-                                     sandbox.altruist_adoption_pool,
+                                     symbolics.altruist_adoption_pool,
                                      modules=[{'ImmutableMatrix': np.array}, "numpy"])
 selfish_adoption_pool = sp.lambdify(tmp_args,
-                                    sandbox.selfish_adoption_pool,
+                                    symbolics.selfish_adoption_pool,
                                     modules=[{'ImmutableMatrix': np.array}, "numpy"])
 
 # wraps the symbolic system and its Jacobian for use in numerical analysis
-tmp_args = (sandbox.men, sandbox.girls, c, dA, da, eA, ea, PiaA, PiAA, Piaa, PiAa)
+tmp_args = (symbolics.men, symbolics.girls, c, dA, da, eA, ea, PiaA, PiAA, Piaa, PiAa)
 model_system = sp.lambdify(tmp_args,
-                           sandbox.model_system,
+                           symbolics.model_system,
                            modules=[{'ImmutableMatrix': np.array}, "numpy"])
 model_jacobian = sp.lambdify(tmp_args,
-                             sandbox.model_jacobian,
+                             symbolics.model_jacobian,
                              modules=[{'ImmutableMatrix': np.array}, "numpy"])
 
 # wraps the symbolic residual and its Jacobian for use in numerical analysis
 residual = sp.lambdify(tmp_args,
-                       sandbox.residual,
+                       symbolics.residual,
                        modules=[{'ImmutableMatrix': np.array}, "numpy"])
 residual_jacobian = sp.lambdify(tmp_args,
-                                sandbox.residual_jacobian,
+                                symbolics.residual_jacobian,
                                 modules=[{'ImmutableMatrix': np.array}, "numpy"])
