67444469ccabb2633c65fdad12c5f9876d51b338
David R. Pugh <drobert.pugh@gmail.com>
2014-09-28 15:58:59 -0400

    Minor mods to accommodate sandbox.

--- examples/random-screening/almost_random_screening_trajectories_1.py
+++ examples/random-screening/almost_random_screening_trajectories_1.py
@@ -1,24 +1,60 @@
 import sys
 sys.path.append('../../')
 
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
+import sympy as sym
 
-from package import model
+from package import sandbox
+
+# number of female children of particular genotype
+girls = sym.DeferredVector('f')
+
+# number of male adults of particular genotype
+men = sym.DeferredVector('M')
+
+# Female signaling probabilities
+female_signaling_probs = sym.var('dA, da')
+dA, da = female_signaling_probs
+
+# Male screening probabilities
+male_screening_probs = sym.var('eG, eg')
+eG, eg = male_screening_probs
+
+# Female population by phenotype.
+altruistic_girls = girls[0] + girls[2]
+selfish_girls = girls[1] + girls[3]
+
+# Conditional phenotype matching probabilities
+true_altruistic_girls = dA * altruistic_girls
+false_altruistic_girls = (1 - da) * selfish_girls
+mistaken_for_altruistic_girls = (1 - eG) * false_altruistic_girls
+altruist_adoption_pool = true_altruistic_girls + mistaken_for_altruistic_girls
+
+true_selfish_girls = da * selfish_girls
+false_selfish_girls = (1 - dA) * altruistic_girls
+mistaken_for_selfish_girls = (1 - eg) * false_selfish_girls
+selfish_adoption_pool = true_selfish_girls + mistaken_for_selfish_girls
+
+SGA = true_altruistic_girls / altruist_adoption_pool
+SGa = 1 - SGA
+Sga = true_selfish_girls / selfish_adoption_pool
+SgA = 1 - Sga
 
 # females send precise signals, but males screen almost randomly
 eps = 1e-3
-params = {'c': 2.0, 'dA': 0.75, 'da': 0.75, 'eG': eps, 'eg': eps,
+params = {'c': 1.0, 'dA': 0.75, 'da': 0.75, 'eG': eps, 'eg': eps,
           'PiaA': 9.0, 'PiAA': 5.0, 'Piaa': 3.0, 'PiAa': 2.0}
 
 # define an array of initial conditions S_A = mGA = fGA
-N_initial = 75
+N_initial = 5
 eps = 1e-3
 mGA0 = np.linspace(eps, 1 - eps, N_initial)
 
 # create an instance of the model
-example = model.Model(params=params,
-                      solver_kwargs={'tol': 1e-12})
+example = sandbox.OneMaleTwoFemalesModel(params=params,
+                                         SGA=SGA,
+                                         Sga=Sga)
 
 fig, axes = plt.subplots(1, 2, figsize=(12, 8))
 
