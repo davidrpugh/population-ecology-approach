2f184ea3c9499f65aa774229eacbf44100275af7
David R. Pugh <drobert.pugh@gmail.com>
2014-09-28 12:48:00 -0400

    Added the iscarrier_X methods and improved the docs.

--- package/sandbox.py
+++ package/sandbox.py
@@ -22,13 +22,19 @@ class Equations(object):
         """
         Number of female children carrying the `A` allele of the alpha gene.
 
+        :getter: Return number of female children carrying the `A` allele
+        :type: sym.Symbol
+
         """
         return girls[0] + girls[2]
 
     @property
     def _selfish_girls(self):
         """
-        Number of female children carrying the a allele of the alpha gene.
+        Number of female children carrying the `a` allele of the alpha gene.
+
+        :getter: Return number of female children carrying the `a` allele
+        :type: sym.Symbol
 
         """
         return girls[1] + girls[3]
@@ -99,7 +105,101 @@ class Equations(object):
 
     def _validate_conditional_prob(self, value):
         """Validate the expression for the conditional matching probability."""
-        if not isinstance(value, sym.basic):
+        if not isinstance(value, sym.Basic):
             raise AttributeError
         else:
             return value
+
+    def iscarrier_G(self, i):
+        """
+        Indicates whether or not adult with genotype i carries the `G` allele.
+
+        Parameters
+        ----------
+        i : int
+            Integer index of a valid genotype.
+
+        Returns
+        -------
+        1 if adult carries the `G` allele, 0 otherwise.
+
+        Notes
+        -----
+        We index genotypes by integers 0, 1, 2, 3 as follows:
+
+            0 = `GA`, 1 = `Ga`, 2 = `gA`, 3 = `ga`.
+
+        """
+        if i in [0, 1]:
+            return 1
+        else:
+            return 0
+
+    def iscarrier_g(self, i):
+        """
+        Indicates whether or not adult with genotype i carries the `g` allele.
+
+        Parameters
+        ----------
+        i : int
+            Integer index of a valid genotype.
+
+        Returns
+        -------
+        1 if adult carries the `g` allele, 0 otherwise.
+
+        Notes
+        -----
+        We index genotypes by integers 0, 1, 2, 3 as follows:
+
+            0 = `GA`, 1 = `Ga`, 2 = `gA`, 3 = `ga`.
+
+        """
+        return 1 - self.iscarrier_G(i)
+
+    def iscarrier_A(self, i):
+        """
+        Indicates whether or not adult with genotype i carries the `A` allele.
+
+        Parameters
+        ----------
+        i : int
+            Integer index of a valid genotype. Must take values 0,1,2,3.
+
+        Returns
+        -------
+        1 if adult carries the `A` allele, 0 otherwise.
+
+        Notes
+        -----
+        We index genotypes by integers 0, 1, 2, 3 as follows:
+
+            0 = `GA`, 1 = `Ga`, 2 = `gA`, 3 = `ga`.
+
+        """
+        if i in [0, 2]:
+            return 1
+        else:
+            return 0
+
+    def iscarrier_a(self, i):
+        """
+        Indicates whether or not adult with genotype i carries the `a` allele.
+
+        Parameters
+        ----------
+        i : int
+            Integer index of a valid genotype.
+
+        Returns
+        -------
+        1 if adult carries the `a` allele, 0 otherwise.
+
+        Notes
+        -----
+        We index genotypes by integers 0, 1, 2, 3 as follows:
+
+            0 = `GA`, 1 = `Ga`, 2 = `gA`, 3 = `ga`.
+
+        """
+        return 1 - self.iscarrier_A(i)
